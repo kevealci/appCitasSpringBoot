4b3cefe1b0095470823645fd5ceb2947e1b9a8cc
Kevin Alcívar <60280778+kevealci@users.noreply.github.com>
2022-01-03 09:49:15 -0500

    Pruebas CORS

--- src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
+++ src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class citasReactivaResource {
 
     @Autowired
