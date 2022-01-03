ed8885eda074135ce2a33b9ffc5cbb37a7ba2365
Kevin Alcívar <60280778+kevealci@users.noreply.github.com>
2022-01-03 10:17:39 -0500

    Pruebas CORS

--- src/main/java/com/springBajo8/springBajo8/SpringBajo8Application.java
+++ src/main/java/com/springBajo8/springBajo8/SpringBajo8Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -25,6 +27,16 @@ public class SpringBajo8Application {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBajo8Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
--- src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
+++ src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
@@ -15,7 +15,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class citasReactivaResource {
 
     @Autowired
